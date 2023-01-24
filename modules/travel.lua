2b69531c04411124661d6fb2338c13edecf8299a
ZelionGG <contact@zeliongg.com>
2023-01-25 00:21:25 +0100

    Removed duplicates.

--- modules/travel.lua
+++ modules/travel.lua
@@ -18,11 +18,6 @@ function TravelModule:OnInitialize()
     193588, -- Timewalker's Hearthstone
     190237, -- Broker Translocation Matrix
     188952, -- Dominated Hearthstone
-    184353, -- Kyrian Hearthstone
-    182773, -- Necrolord Hearthstone
-    180290, -- Night Fae Hearthstone
-    183716, -- Venthyr Sinstone
-    172179, -- Eternal Travaler's Hearthstone
     28585,  -- Ruby Slippers
     54452,  -- Ethereal Portal
     93672,  -- Dark Portal
